d7ca123994712b1edb43299c5bdb48768ca446f5
Gaurav Chandak <gaurav01101993@gmail.com>
2021-03-03 00:54:32 +0530

    Remove console.log

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use('/api', api);
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', function (req, res) {
-  console.log(req.path);
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
